be52e5224feabc820867975330d5b0cd4125c64e
CodedCells <codedcells@gmail.com>
2020-05-15 22:09:44 +0100

    merge mark and seen
    they both js

--- mark.js
+++ mark.js
@@ -27,15 +27,63 @@ function markback() {
 	}
 }
 
-function rebuild_handler() {
+function pRHandler() {
 	var data = JSON.parse(this.responseText);
 	if (data.status == "success") {
 		location.reload();
 	} else {
-		alert(data);
+		console.log(data);
+		alert(data.message);
 	}
 }
 
-function rebuild() {
-	send_xhr('/rebuild', '', rebuild_handler)
+function postReload(addr, ctn) {
+	send_xhr(addr, ctn, pRHandler);
+}
+
+function showCYOA() {
+	if (this.responseText == undefined) {this.responseText = "{'artists': {}}"}
+	var entries = JSON.parse(this.responseText);
+	if (entries['results'] == 0) {return;}
+	var cont = document.getElementsByClassName("container")[0];
+	cont.innerHTML = "<h2>Unknown Length (" + entries['results'] + ")</h2>";
+	entries = entries['artists'];
+	
+	for (artist in entries) {
+		e = '<div class="thumbw">'
+		e += entries[artist];
+		e += '<input class="niceinp" id="'+artist+'ip" type="number">\n';
+		e += '<button id="'+artist+'l8r" onclick="l8r(\'' + artist + '\')">l8r</button>\n';
+		e += '</div>\n';
+		cont.innerHTML += e;
+	}
+}
+
+function getCYOA(count) {
+	send_xhr('/cyoa', JSON.stringify({"count": count}), showCYOA);
+}
+
+function l8r(artist) {
+	no = parseInt(document.getElementById(artist+'ip').value);
+	mark(artist, 'l8r', no)
+}
+
+function set() {
+	if (this.responseText == undefined) {this.responseText = "{}";}
+	var entries = JSON.parse(this.responseText);
+	var cont = document.getElementsByClassName("container")[0];
+	cont.innerHTML = "<h2>" + Object.keys(entries).length + " result(s).</h2><br>"
+	
+	for (artist in entries) {
+		cont.innerHTML += entries[artist];
+	}
+}
+
+function search(force) {
+	var query = document.getElementsByTagName("input")[0].value.toLowerCase();
+	if (query.length < 3 && !(force))
+		return;
+	if (query.length == 0) {set();return;}
+	var meta = {"query": query}
+	send_xhr('/search', JSON.stringify(meta), set);
 }
